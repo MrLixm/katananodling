3f083031d0cd8aa0a2ee18ec895c2bd4684873d2
Liam Collod <lcollod@gmail.com>
2022-09-04 18:53:48 +0200

    upgrade version to 1.0.0

--- katananodling/c.py
+++ katananodling/c.py
@@ -1,8 +1,8 @@
 """
 Constants
 """
-__version_major__ = 0
-__version_minor__ = 1
+__version_major__ = 1
+__version_minor__ = 0
 __version_patch__ = 0
 __version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_patch__)
 
