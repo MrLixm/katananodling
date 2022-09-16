c08480059211a309da2f8b7ff733e65310ea4041
Liam Collod <lcollod@gmail.com>
2022-09-16 20:12:15 +0200

    upgraded version from 1.1.3 to 1.1.4

--- katananodling/c.py
+++ katananodling/c.py
@@ -3,7 +3,7 @@ Constants
 """
 __version_major__ = 1
 __version_minor__ = 1
-__version_patch__ = 3
+__version_patch__ = 4
 __version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_patch__)
 
 from typing import Any
